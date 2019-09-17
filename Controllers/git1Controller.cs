76d5c4ad545a206820f4c18f9a6781c9374221c1
Mohammad Nahid Mahmood <nahidmmahmood35@gmail.com>
2019-09-17 15:14:36 +0600

    9th commit

---
+++ Controllers/git1Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HospitalManagementApp_Api.Controllers
+{
+    public class git1Controller : Controller
+    {
+        //
+        // GET: /git1/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
